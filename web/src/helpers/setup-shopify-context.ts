572d5a0e3d125b68ba090bee575692b221f67cfa
Morgan O'Neal <morgan@ghostbyte.com>
2022-09-25 11:41:24 -0700

    default to use firebase for sessions

--- web/src/helpers/setup-shopify-context.ts
+++ web/src/helpers/setup-shopify-context.ts
@@ -1,5 +1,5 @@
 import { Shopify, LATEST_API_VERSION } from "@shopify/shopify-api";
-// import { FirebaseSessionStore } from "./firestore-session";
+import { FirebaseSessionStore } from "./firestore-session";
 
 export const shopifyContextAvailable = () => {
   return typeof Shopify !== "undefined";
@@ -19,7 +19,7 @@ export const setupShopifyContext = () => {
     throw new Error("Environment variable HOST is not set");
   }
 
-  // const sessionStorage = new FirebaseSessionStore();
+  const sessionStorage = new FirebaseSessionStore();
   Shopify.Context.initialize({
     API_KEY: process.env.SHOPIFY_API_KEY,
     API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
@@ -30,16 +30,16 @@ export const setupShopifyContext = () => {
     IS_EMBEDDED_APP: true,
     // This should be replaced with your preferred storage strategy
     // SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
-    SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(
-      `${process.cwd()}/database.sqlite`
-    ),
-    // SESSION_STORAGE: new Shopify.Session.CustomSessionStorage(
-    //   sessionStorage.storeCallback.bind(sessionStorage),
-    //   sessionStorage.loadCallback.bind(sessionStorage),
-    //   sessionStorage.deleteCallback.bind(sessionStorage),
-    //   sessionStorage.deleteSessionsCallback.bind(sessionStorage),
-    //   sessionStorage.findSessionsByShopCallback.bind(sessionStorage)
+    // SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(
+    //   `${process.cwd()}/database.sqlite`
     // ),
+    SESSION_STORAGE: new Shopify.Session.CustomSessionStorage(
+      sessionStorage.storeCallback.bind(sessionStorage),
+      sessionStorage.loadCallback.bind(sessionStorage),
+      sessionStorage.deleteCallback.bind(sessionStorage),
+      sessionStorage.deleteSessionsCallback.bind(sessionStorage),
+      sessionStorage.findSessionsByShopCallback.bind(sessionStorage)
+    ),
     LOG_FILE: "shopify-api.log",
     // SESSION_STORAGE: new Shopify.Session.SQLiteSessionStorage(DB_PATH),
   });
